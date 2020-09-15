b8d562d0ba636dbfc425c352cc6b813d714d84b4
Damien Danglard <ddanglard@cegid.com>
2020-09-15 10:11:19 +0200

    Initial Commit

---
+++ MaterialDesignIcons/MaterialDesignIcons.h
@@ -0,0 +1,19 @@
+//
+//  MaterialDesignIcons.h
+//  MaterialDesignIcons
+//
+//  Created by Damien Danglard on 15/09/2020.
+//  Copyright © 2020 CEGID. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaterialDesignIcons.
+FOUNDATION_EXPORT double MaterialDesignIconsVersionNumber;
+
+//! Project version string for MaterialDesignIcons.
+FOUNDATION_EXPORT const unsigned char MaterialDesignIconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialDesignIcons/PublicHeader.h>
+
+
